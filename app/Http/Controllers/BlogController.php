288f760d2b2b869c580c0fc451d737575d3d9875
tibohumblet <tibohumblet@hotmail.com>
2026-08-27 19:43:06 +0000

    update Blog controller
    show func added: view stil needs to be created!

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class BlogController extends Controller
 {
@@ -11,6 +12,15 @@ class BlogController extends Controller
     {
         return view('blog.index', [
             'blogs' => Blog::all()
+        ]);
+    }
+
+    // Doc: Wiew needs to be created!
+    
+    public function show($id): View
+    {
+        return view('blog.show', [
+            'blog' => Blog::findOrFail($id)
         ]);
     }
 }
